662cb7cfb6c16b5412bd546c450b3c386032c32e
superwaltz <lzyzllzy@163.com>
2020-09-29 11:11:39 +0800

    update controller

--- src/main/java/com/liam/webhook/WebhookController.java
+++ src/main/java/com/liam/webhook/WebhookController.java
@@ -1,7 +1,6 @@
 package com.liam.webhook;
 
 import org.apache.commons.io.IOUtils;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
